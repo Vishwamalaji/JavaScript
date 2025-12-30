e23a2c834d9e408fdf29c9da36c37b4ba514623d
Vishwamalaji <vishwamalji@gmail.com>
2025-12-30 13:26:31 +0000

    container configure done

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const phoneNum = 121212;
+let userName = "vishwa";
+var gmail = "vishwa@google.com";
+
+
+phoneNum = 454545
+userName = "malji"
+gmail = "malji@google.com"
+console.log(phoneNum, userName, gmail);
+
+
+
