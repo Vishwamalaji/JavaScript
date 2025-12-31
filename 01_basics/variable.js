4f83e05324f9a217e1e59b152645144f4efe310d
Vishwamalaji <vishwamalji@gmail.com>
2025-12-31 04:40:12 +0000

    variables

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -3,6 +3,10 @@ let userName = "vishwa";
 var gmail = "vishwa@google.com";
 
 
+/*
+prefer not to use var bcoz of block scope issue
+*/
+
 phoneNum = 454545
 userName = "malji"
 gmail = "malji@google.com"
