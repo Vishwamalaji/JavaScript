a1895ce43f8fc38530dd284fca34fd9c2bedde14
Vishwamalaji <vishwamalji@gmail.com>
2025-12-31 05:03:00 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOpeartion.js
@@ -0,0 +1,21 @@
+// let score = false;
+
+// console.log(typeof score)
+
+// let Num = Number(score);
+// console.log(typeof Num)
+// console.log(Num)
+
+
+
+// let isFollow = "Vishwa";
+
+// let bool = Boolean(isFollow);
+// console.log(bool)
+
+
+// let num = 45;
+// console.log(typeof num)
+
+// let stringNum = String(num);
+// console.log(typeof stringNum)
