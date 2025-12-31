2257b9028b4df41b35d45645adb7af49a6a616c1
Vishwamalaji <vishwamalji@gmail.com>
2025-12-31 04:39:49 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+// let name = "vishwa";
+
+// console.log(name)
+
+//String => name
+//number => number
+//boolean => true/false
+//bigint => for big numbers
+//symbol => unique
+//null
+//undefined
+
+//object ==> non-premitive
+
+
+console.log(typeof undefined)
