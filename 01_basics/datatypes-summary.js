262fa22052fcce9a47c0281c76c394e41401713f
Vishwamalaji <vishwamalji@gmail.com>
2026-01-07 04:16:51 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,10 +10,10 @@ const mySymbol = Symbol('123')
 const yourSymbol = Symbol('123')
 // console.log(mySymbol === yourSymbol);
 
-console.log(typeof mySymbol)
+// console.log(typeof mySymbol)
 
 // Non-primitive
-// objects, arrays, References
+// objects, arrays,function
 
 let student = {
     name:"vishwa",
@@ -23,11 +23,37 @@ let student = {
 
 let characters = ["mike","will","lucus","dustibun","steve"];
 
-console.log(typeof characters)
+// console.log(typeof characters)
 
 let myFunction = function(){
-    console.log("Hello world!");
+    // console.log("Hello world!");
 }
 
 myFunction();
-console.log(typeof myFunction)
+// console.log(typeof myFunction)
+
+
+// **************************************
+
+// stack(premitive) and heap(non-premitive) memory
+
+let myName = "vishwa";
+let anotherName = myName;
+
+anotherName = "vishwambhar"
+
+console.log(myName);
+console.log(anotherName);
+
+let myObj = {
+    name:"vishwa",
+    upi:"vishwa@fam"
+}
+// console.log(myObj);
+
+let myObj2 = myObj;
+// console.log(myObj2)
+
+myObj2.upi = "vishwambhar@irl";
+console.log(myObj)
+console.log(myObj2)
