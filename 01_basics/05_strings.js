ae8fb47e49a283f1faa5ae40d5fcc91f2df5ebd0
Vishwamalaji <vishwamalji@gmail.com>
2026-01-08 03:56:34 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+let name = "Vishwa";
+let cgpa = 9.5;
+
+// console.log(name + cgpa + "abc")
+
+// string interpolation
+// console.log(`my name is ${name} and my cgpa is ${cgpa}`)
+
+
+let gameName = new String("vishwa")
+console.log(gameName[4])
